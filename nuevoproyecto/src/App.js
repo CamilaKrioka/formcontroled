5e1f56232fd0ecd724250c01d284379cc62476e5
CamilaKrioka <camilamiki96@gmail.com>
2020-07-29 15:17:31 -0300

    firstcommit

---
+++ nuevoproyecto/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './App.css';
+
+import MyForm from './Form';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <MyForm />
+
+ 
+
+    
+  
+
+       </div>
+  );
+    
+   
+  
+}
+
+export default App;
